f326b04f97ff0ffb8b033767d48159661c7d08d9
eliana-flo <66032622+eliana-flo@users.noreply.github.com>
2025-11-21 09:58:46 -0500

    Create mediaByLocation.sql

---
+++ Wentworth/mediaByLocation.sql
@@ -0,0 +1,21 @@
+--metadb:function mediaByLocation
+
+DROP FUNCTION IF EXISTS mediaByLocation;
+
+CREATE FUNCTION mediaByLocation()
+RETURNS TABLE
+  (location text,
+  item_type text,
+  holdings_count integer)
+AS $$
+SELECT lt.name as Location, mtt.name as item_type, COUNT (it.id) as holdings_count
+FROM folio_inventory.item__t it
+LEFT JOIN folio_inventory.location__t lt ON (lt.id = it.effective_location_id)
+LEFT JOIN folio_inventory.material_type__t mtt ON (mtt.id = it.material_type_id)
+WHERE mtt.name NOT LIKE 'Book'
+GROUP BY lt.name, mtt.name
+ORDER BY lt.name, mtt.name
+$$
+LANGUAGE SQL
+STABLE
+PARALLEL SAFE;
