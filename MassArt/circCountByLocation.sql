4b7e53e4f646df1cd75424559a02012c6a0281ac
eliana-flo <66032622+eliana-flo@users.noreply.github.com>
2025-11-24 13:38:54 -0500

    Create circCountByLocation.sql

---
+++ MassArt/circCountByLocation.sql
@@ -0,0 +1,52 @@
+--metadb:function circCountByLocation
+
+DROP FUNCTION IF EXISTS circCountByLocation;
+
+CREATE FUNCTION circCountByLocation(
+    shelving_location text DEFAULT '')
+RETURNS TABLE(
+    title text,
+    contributor_name text,
+    publisher text,
+  date_of_publication text,
+  effective_call_number text,
+  effective_location_name text,
+  folio_charges integer,
+  voyager_charges text
+  )
+AS $$
+with inst_contributors as (
+  select ic.instance_id, ic.contributor_name
+  from folio_derived.instance_contributors ic where ic.contributor_is_primary='TRUE'
+  group by ic.instance_id,ic.contributor_name
+  ),
+  inst_publishers as (
+  select ip.instance_id, ip.publisher, ip.date_of_publication
+  from folio_derived.instance_publication ip where ip.publication_ordinality='1'
+  group by ip.instance_id, ip.publisher, ip.date_of_publication)
+select
+  it.title,
+  ic2.contributor_name, 
+  ip2.publisher,
+  ip2.date_of_publication,
+  ie.effective_call_number, 
+  ie.effective_location_name,
+  count(li.id) as folio_charges,
+  io.note as voyager_charges
+from folio_derived.item_ext ie 
+left join folio_inventory.item__t it2 on (ie.item_id = it2.id) 
+left join folio_derived.item_notes io on (ie.item_id = io.item_id) 
+left join folio_inventory.holdings_record__t hrt on (ie.holdings_record_id = hrt.id)
+left join folio_inventory.location__t__ lt on (hrt.permanent_location_id = lt.id)
+left join folio_inventory.instance__t it on (hrt.instance_id = it.id)
+left join inst_contributors ic2 on (it.id = ic2.instance_id)
+left join inst_publishers ip2 on (it.id = ip2.instance_id)
+left join folio_circulation.loan__t__ li on (li.item_id = ie.item_id)
+where io.note_type_name = 'Voyager Historical Charges' and ie.effective_location_name = shelving_location
+group by it.title, ic2.contributor_name, ip2.publisher, ip2.date_of_publication, it2.effective_shelving_order, ie.effective_call_number, ie.effective_location_name, io.note
+order by it2.effective_shelving_order
+$$
+LANGUAGE SQL
+STABLE
+PARALLEL SAFE;
+
