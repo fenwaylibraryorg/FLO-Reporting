cfc6f1debd02689797df40932ff79d1a44daf635
eliana-flo <66032622+eliana-flo@users.noreply.github.com>
2025-12-04 13:14:28 -0500

    Update noticesByDate.sql

--- Berklee/noticesByDate.sql
+++ Berklee/noticesByDate.sql
@@ -6,15 +6,15 @@ CREATE FUNCTION noticesByDate(
   start_date date DEFAULT '2000-01-01',
   end_date date DEFAULT '2050-01-01')
 RETURNS TABLE
-  (date_sent date,
+  (date_sent text,
   notice_type text,
   notice_count integer)
 AS $$
-select cast(est.date as date) as "date sent", est."header" as "notice type", count(est.id)
+select est.date::date::text as "date sent", est."header" as "notice type", count(est.id)
 from folio_email.email_statistics__t__ est
-where cast(est.date as date) between '2025-12-01' and '2025-12-04'
-group by cast(est.date as date), est."header"
-order by cast(est.date as date), est."header"
+where est.date::date between '2025-12-01' and '2025-12-04'
+group by est.date::date, est."header"
+order by est.date::date, est."header"
 $$
 LANGUAGE SQL
 STABLE
