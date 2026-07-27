7b827672f82549e3fa197e1540c81129761babad
jennflo <113996307+jennflo@users.noreply.github.com>
2026-07-27 17:04:48 -0400

    Update annual-circ-count-by-location.sql

--- MFA/annual-circ-count-by-location.sql
+++ MFA/annual-circ-count-by-location.sql
@@ -15,8 +15,8 @@ select lt2.name as "shelving_location", count(lt.jsonb->'action') as "circ_count
 from folio_circulation.loan__ lt
 join folio_inventory.location__t lt2 ON ((lt.jsonb->>'itemEffectiveLocationIdAtCheckOut') :: uuid = lt2.id)
 where lt.jsonb->>'action' in ('checkedout','renewed','checkedOutThroughOverride', 'renewedThroughOverride')
-and (jsonb_extract_path_text(lt.jsonb, 'metadata', 'updatedDate' ) :: timestamp >= 'start_date 0:00:00+00'
-and jsonb_extract_path_text(lt.jsonb, 'metadata', 'updatedDate' ) :: timestamp <= 'end_date 0:00:00+00')
+and (jsonb_extract_path_text(lt.jsonb, 'metadata', 'updatedDate' ) :: timestamp >= start_dat || '0:00:00+00'
+and jsonb_extract_path_text(lt.jsonb, 'metadata', 'updatedDate' ) :: timestamp <= end_date || '0:00:00+00')
 group by rollup(lt2.name)
 order by lt2.name nulls last
 $$
