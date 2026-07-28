d3bccbd03c112c7e61b3cbda8baceda5d103fd0f
jennflo <113996307+jennflo@users.noreply.github.com>
2026-07-28 10:55:45 -0400

    Update annual-circ-count-by-location.sql

--- MFA/annual-circ-count-by-location.sql
+++ MFA/annual-circ-count-by-location.sql
@@ -4,8 +4,8 @@
 DROP FUNCTION IF EXISTS circCount;
 
 CREATE FUNCTION circCount(  
-  start_date timestamp DEFAULT '2000-01-01 0:00:00+00',
-  end_date timestamp DEFAULT '2050-01-01 0:00:00+00')
+  start_date timestamptz DEFAULT '2000-01-01 0:00:00+00',
+  end_date timestamptz DEFAULT '2050-01-01 0:00:00+00')
 RETURNS TABLE(
   shelving_location text,
   circ_count integer
@@ -15,8 +15,8 @@ select lt2.name as "shelving_location", count(lt.jsonb->'action') as "circ_count
 from folio_circulation.loan__ lt
 join folio_inventory.location__t lt2 ON ((lt.jsonb->>'itemEffectiveLocationIdAtCheckOut') :: uuid = lt2.id)
 where lt.jsonb->>'action' in ('checkedout','renewed','checkedOutThroughOverride', 'renewedThroughOverride')
-and (jsonb_extract_path_text(lt.jsonb, 'metadata', 'updatedDate' ) :: timestamp >= CONCAT(start_dat, '0:00:00+00')
-and jsonb_extract_path_text(lt.jsonb, 'metadata', 'updatedDate' ) :: timestamp <= CONCAT(end_date , '0:00:00+00'))
+and (jsonb_extract_path_text(lt.jsonb, 'metadata', 'updatedDate' ) :: timestamp >=start_date
+and jsonb_extract_path_text(lt.jsonb, 'metadata', 'updatedDate' ) :: timestamp <= end_date)
 group by rollup(lt2.name)
 order by lt2.name nulls last
 $$
